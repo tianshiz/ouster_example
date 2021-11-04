83bde4a98bcee171bacdf63b0a7e2460ea3e75de
root <lz233@cornell.edu>
2021-11-04 19:39:50 +0000

    tested working with pcap file, fixed timing issues

--- ouster_ros/src/pcap_pub_node.py
+++ ouster_ros/src/pcap_pub_node.py
@@ -8,8 +8,12 @@ from ouster import client, pcap
 from ouster_ros.msg import PacketMsg
 from sensor_msgs.msg import PointCloud2
 import numpy as np
+def nano_time():
+    now = rospy.Time.now()
+    return now.nsecs + now.secs * 1e9
 if __name__ == '__main__':
     rospy.init_node('pcap_pub_node')
+    rate = rospy.Rate(10000000)
     meta_path = rospy.get_param("~meta_path")
     pcap_path = rospy.get_param("~pcap_path")
     imu_pub = rospy.Publisher('/os1_node/imu_packets', PacketMsg, queue_size=10)
@@ -17,14 +21,29 @@ if __name__ == '__main__':
     with open(meta_path, 'r') as f:
         metadata = client.SensorInfo(f.read())
     source = pcap.Pcap(pcap_path, metadata)
+    start_time = None
+    start_stamp = None
+    timestamps = None
     for packet in source:
         if isinstance(packet, client.LidarPacket):
-            # Now we can process the LidarPacket. In this case, we access
-            # the encoder_counts, timestamps, and ranges
-            lidar_msg = PacketMsg()
-            lidar_msg.buf =packet._data.tolist()
-            lidar_pub.publish(lidar_msg)
+            timestamps = packet.header(client.ColHeader.TIMESTAMP)[0]
         elif isinstance(packet, client.ImuPacket):
-            imu_msg = PacketMsg()
-            imu_msg.buf = packet._data.tolist()
-            imu_pub.publish(imu_msg)
+            timestamps = packet.gyro_ts
+        
+        if start_time:
+            #seconds since start from packet
+            diff = (timestamps-start_stamp)                 
+            #seconds since start of this script
+            diff_clock = (nano_time()-start_time)
+            #both time should be aligned to simulate realtime playback
+            rospy.sleep((diff-diff_clock)/1e9)
+        else:
+            start_time = nano_time()
+            start_stamp = timestamps
+        msg = PacketMsg()
+        msg.buf = packet._data.tolist()
+        if isinstance(packet, client.LidarPacket):
+            lidar_pub.publish(msg)
+        elif isinstance(packet,client.ImuPacket):
+            imu_pub.publish(msg)
+    print("completed {} secs of data.".format((nano_time()-start_time)/1e9))
